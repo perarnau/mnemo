fff81c59510c7fac9dbe058067ae2db01683e915
Swann Perarnau <swann@anl.gov>
2020-09-18 12:04:13 -0500

    [fix] fix imports in python bindings

--- bindings/python/mnemo/reuse.py
+++ bindings/python/mnemo/reuse.py
@@ -15,22 +15,22 @@ def _mn_get_function(method, argtypes=[], restype=mn_result):
     res.argtypes = argtypes
     return res
 
-__libmn_reusedm_init = getfunction("mnemo_reusedm_init", [mn_size], mn_reusedm)
-__libmn_reusedm_add = getfunction("mnemo_reusedm_add", [mn_reusedm, mn_key])
-__libmn_reusedm_reset = getfunction("mnemo_reusedm_reset", [mn_reusedm], None)
-__libmn_reusedm_fini = getfunction("mnemo_reusedm_fini", [mn_reusedm], None)
+libmn_reusedm_init = _mn_get_function("mnemo_reusedm_init", [mn_size], mn_reusedm)
+libmn_reusedm_add = _mn_get_function("mnemo_reusedm_add", [mn_reusedm, mn_key])
+libmn_reusedm_reset = _mn_get_function("mnemo_reusedm_reset", [mn_reusedm], None)
+libmn_reusedm_fini = _mn_get_function("mnemo_reusedm_fini", [mn_reusedm], None)
 
 class ReuseDM():
 
     def __init__(self, maxsize=0):
-        self.handle = __libmn_reusedm_init(maxsize)
+        self.handle = libmn_reusedm_init(maxsize)
 
     def add(self, key):
-        return __libmn_reusedm_add(self.handle, key)
+        return libmn_reusedm_add(self.handle, key)
 
     def reset(self):
-        __libmn_reusedm_reset(self.handle)
+        libmn_reusedm_reset(self.handle)
 
     def __del__(self):
-        __libmn_reusedm_fini(self.handle)
+        libmn_reusedm_fini(self.handle)
 
