2493cc9a59360127009fdbc02fc89847256ae8b2
Swann Perarnau <swann@anl.gov>
2020-09-21 11:16:30 -0500

    [fix] typo in env var name

--- bindings/python/mnemo/__init__.py
+++ bindings/python/mnemo/__init__.py
@@ -2,7 +2,8 @@ import ctypes as ct
 from ctypes.util import find_library
 import os
 
-mnemopath = os.environ.get("LIBMNENO_SO", find_library("mnemo"))
+mnemopath = os.environ.get("LIBMNEMO_SO", find_library("mnemo"))
+assert mnemopath is not None
 libmnemo = ct.cdll.LoadLibrary(mnemopath)
 
 from .reuse import *
